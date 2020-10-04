dad88cedbdb827b85638ea9323aa166e3858d99b
Hung Nguyen <hung.nguyen@bionova.fi>
2020-10-04 17:20:50 +0300

    Add disclaimer popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import axios from 'axios'
 
-import { Transition } from './components'
+import { Transition, Popup } from './components'
 import { CreateGame, FunctionBar, GameCard, GameSelect, NavBar } from './containers'
 import { loadAll } from './redux/actions'
 import { AppState, GameName } from './types'
@@ -10,6 +10,7 @@ import './tailwind.output.css'
 
 export default function App() {
   const dispatch = useDispatch()
+  const [showPopup, displayPopup] = useState(true)
   const showGameCard = useSelector((state: AppState) => state.pokerBoard.showGameCard)
   const showCreateGame = useSelector((state: AppState) => state.pokerBoard.showCreateGame)
   const showGameSelect = useSelector((state: AppState) => state.pokerBoard.showGameSelect)
@@ -25,6 +26,16 @@ export default function App() {
   return (
     <div className="md:flex md:justify-center">
       <div className="md:max-w-screen-md md:w-full md:bg-gray-100 min-h-screen">
+        <Transition showCondition={showPopup}>
+          <Popup
+            title="Disclaimer"
+            message="Poker Board is a free tool available to the world. By continuing you agree to publicly share any data posted to Poker Board. It means everyone will know how bad you are at poker :)"
+            confirmBtnLabel="I agree to share my data"
+            confirmHandler={() => {
+              displayPopup(false)
+            }}
+          />
+        </Transition>
         <NavBar />
         <Transition showCondition={showCreateGame}>
           <CreateGame />
