631c89d9492fc059b4f0a22dc64c1231ac0423f7
Hung Nguyen <hung.nguyen@bionova.fi>
2020-10-04 17:38:06 +0300

    center alert and popup

--- src/components/Popup/index.tsx
+++ src/components/Popup/index.tsx
@@ -14,7 +14,7 @@ export const Popup = ({ title, message, confirmBtnLabel, confirmHandler }: Props
         <div className="fixed inset-0 transition-opacity">
           <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
         </div>
-        {/* <span className="hidden sm:inline-block sm:align-middle sm:h-screen"></span>&#8203; */}
+        <span className="hidden sm:inline-block sm:align-middle sm:h-screen"></span>&#8203;
         <div
           className="w-5/6 inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
           role="dialog"
